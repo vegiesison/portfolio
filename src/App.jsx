0d25fb8ea73de428f73df10cb517262b7ff31180
rhojane <rlusterio@unixsurplus.com>
2022-12-07 16:56:38 +0800

    added all initial section of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contacts from './components/contacts/Contacts'
+import Footer from './components/footer/Footer'
+
+
 
 
 
@@ -9,6 +18,13 @@ const App = () => {
     <>
       <Header />
       <Nav />
+      <About />
+      <Experience />
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contacts/>
+      <Footer/>
     </>
   )
 }
