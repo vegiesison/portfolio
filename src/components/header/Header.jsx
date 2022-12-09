606d3d7f0dfd57e00ae8eb166a08e76df5ecf8ba
Vegie <vegiesison@gmail.com>
2022-12-09 14:11:50 +0800

    Changes in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Vegie Sison</h1>
+        <h5 className="text-light">FullStack Developer</h5>
+        <CTA/>
+      </div>
+      <div className='me'>
+        <img src={ME} alt="me" />
+      </div>
+    </header>
   )
 }
 
